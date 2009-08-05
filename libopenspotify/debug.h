681be1abd8c8e9e3818777c623b9184e2c0c6bee
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-05 03:22:17 +0200

    Make debug.h more readble

--- libopenspotify/debug.h
+++ libopenspotify/debug.h
@@ -3,9 +3,30 @@
 
 #ifdef DEBUG
 #ifdef _WIN32
-#define DSFYDEBUG(...) { FILE *fd = fopen("despotify.log","at"); fprintf(fd, "%s:%d %s() ", __FILE__, __LINE__, __FUNCTION__); fprintf(fd, __VA_ARGS__); fclose(fd); }
+#define DSFYDEBUG(...) {                                        \
+        FILE *fd;                                               \
+        char *file = __FILE__, *ptr;                            \
+                                                                \
+        for(ptr = file; *ptr; ptr++);                           \
+        while(ptr != file && *ptr-- != '\\');                   \
+        if(ptr != file) ptr += 2;                               \
+        if((fd = fopen("despotify.log", "at")) != NULL) {       \
+                fprintf(fd, "%s:%d %s() ", ptr, __LINE__,       \
+                        __FUNCTION__);                          \
+                fprintf(fd, __VA_ARGS__);                       \
+                fclose(fd);                                     \
+	}							\
+}
 #else
-#define DSFYDEBUG(...) { FILE *fd = fopen("/tmp/despotify.log","at"); fprintf(fd, "%s:%d %s() ", __FILE__, __LINE__, __func__); fprintf(fd, __VA_ARGS__); fclose(fd); }
+#define DSFYDEBUG(...) {                                        \
+        FILE *fd;                                               \
+        if((fd = fopen("despotify.log", "at")) != NULL) {       \
+                fprintf(fd, "%s:%d %s() ", __FILE__, __LINE__,	\
+                        __func__);				\
+                fprintf(fd, __VA_ARGS__);			\
+                fclose(fd);					\
+        }							\
+}
 #endif
 #else
 #define DSFYDEBUG(...)
