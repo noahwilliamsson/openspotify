8933e147ba369a52ed18bcfa6524ee56a83cc81a
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-06 00:57:40 +0200

    really fix the protocol violation

--- libopenspotify/commands.c
+++ libopenspotify/commands.c
@@ -61,7 +61,7 @@ int cmd_send_cache_hash (sp_session * session)
 	/* FIXME */
 	char cache_hash[] = "\xf4\xc2\xaa\x05\xe8\x25\xa7\xb5\xe4\xe6\x59\x0f\x3d\xd0\xbe\x0a\xef\x20\x51\x95";
 
-	buf_append_data(buf, cache_hash, sizeof (cache_hash));
+	buf_append_data(buf, cache_hash, 20);
 
 	ret = packet_write (session, 0x0f, buf->ptr, buf->len);
 	buf_free(buf);
