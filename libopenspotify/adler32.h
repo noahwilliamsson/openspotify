06fa6e175514b5981ca42705c0dde87add85adcb
Felix Bruns <felixbruns@web.de>
2009-08-07 16:04:39 +0200

    Add zlib to include path.

--- libopenspotify/adler32.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADLER32_H
-#define ADLER32_H
-
-unsigned long adler32_update(unsigned long adler, const unsigned char *data, int len);
-unsigned long adler32(unsigned char *data, int len);
-
-#endif /* ADLER32_H */
