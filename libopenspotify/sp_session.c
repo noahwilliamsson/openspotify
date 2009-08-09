49d53531b151bfb4f191d5db9599ba0d377169d6
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-09 18:34:14 +0200

    Do not release the session's playlists on logout

--- libopenspotify/sp_session.c
+++ libopenspotify/sp_session.c
@@ -134,10 +134,6 @@ SP_LIBEXPORT(sp_error) sp_session_logout (sp_session *session) {
 		session->login = NULL;
 	}
 
-	if(session->playlist_ctx) {
-		playlist_release(session->playlist_ctx);
-		session->playlist_ctx = playlist_create();
-	}
 
 	DSFYDEBUG("Posting REQ_TYPE_LOGOUT\n");
 	request_post(session, REQ_TYPE_LOGOUT, NULL);
