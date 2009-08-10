15e06a8aac9497793ee4ce68c54c16e6db186ee1
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-10 10:22:45 +0200

    Disable save/restore of track metadata until all the loose ends are sorted out

--- libopenspotify/cache.c
+++ libopenspotify/cache.c
@@ -7,7 +7,9 @@
 
 
 void cache_init(sp_session *session) {
+#if 0
 	osfy_track_metadata_load_from_disk(session, "tracks.cache");
+#endif
 }
 
 
@@ -15,8 +17,10 @@ int cache_process(sp_session *session, struct request *req) {
 	/* Do some garbage collection */
 	osfy_track_garbage_collect(session);
 
+#if 0
 	/* Save metadata to disk */
 	osfy_track_metadata_save_to_disk(session, "tracks.cache");
+#endif
 
 	req->next_timeout = get_millisecs() + 5*60*1000;
 
