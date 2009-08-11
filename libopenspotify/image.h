afe62f6cda435d9e72dcb61e756d36872a3bbb3a
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-11 20:52:12 +0200

    Fix function name typo in image.h

--- libopenspotify/image.h
+++ libopenspotify/image.h
@@ -15,7 +15,7 @@ struct image_ctx {
 };
 
 
-sp_image *ofsy_image_create(sp_session *session, const byte image_id[20]);
+sp_image *osfy_image_create(sp_session *session, const byte image_id[20]);
 int osfy_image_process_request(sp_session *session, struct request *req);
 
 #endif
