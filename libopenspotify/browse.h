dfc745d620ad31430ce88ba92007c1289061c0fc
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-11 19:10:06 +0200

    Include spotify/api.h and request.h instead of sp_opaque.h

--- libopenspotify/browse.h
+++ libopenspotify/browse.h
@@ -1,10 +1,14 @@
 #ifndef LIBOPENSPOTIFY_BROWSE_H
 #define LIBOPENSPOTIFY_BROWSE_H
 
+#include <spotify/api.h>
+
 #include "buf.h"
-#include "sp_opaque.h"
+#include "request.h"
+
 
 #define BROWSE_RETRY_TIMEOUT	30
+
 
 int browse_process(sp_session *session, struct request *req);
 
