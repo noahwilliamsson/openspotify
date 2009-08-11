ad824f17d93cf6903bf1d038f3397e8dc7c9573a
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-11 19:14:11 +0200

    Add a more generic browse handler/callback

--- libopenspotify/browse.c
+++ libopenspotify/browse.c
@@ -45,6 +45,8 @@
 
 
 
+static int browse_send_generic_request(sp_session *session, struct request *req);
+static int browse_generic_callback(CHANNEL *ch, unsigned char *payload, unsigned short len);
 static int browse_send_browsetrack_request(sp_session *session, struct request *req, int offset);
 static int browse_callback(CHANNEL *ch, unsigned char *payload, unsigned short len);
 static int browse_parse_compressed_xml(sp_session *session, sp_playlist *playlist);
@@ -64,39 +66,182 @@ struct callback_ctx {
 int browse_process(sp_session *session, struct request *req) {
 	int ret;
 
-	if(req->type == REQ_TYPE_BROWSE_TRACK) {
-		req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
+	
+	req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
 
-		/* Send request (CMD_BROWSE) to load track data. */
-		ret = browse_send_browsetrack_request(session, req, 0);
-	}
-	else if(req->type == REQ_TYPE_BROWSE_ALBUM) {
-		req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
-		
-		DSFYDEBUG("REQ_TYPE_BROWSE_ALBUM is not yet implemented\n");
-		return request_set_result(session, req, SP_ERROR_OTHER_PERMAMENT, NULL);
-	}
-	else if(req->type == REQ_TYPE_BROWSE_ARTIST) {
-		req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
-		
-		DSFYDEBUG("REQ_TYPE_BROWSE_ARTIST is not yet implemented\n");
-		return request_set_result(session, req, SP_ERROR_OTHER_PERMAMENT, NULL);
-	}
-	else if(req->type == REQ_TYPE_ALBUMBROWSE) {
-		req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
-		
-		DSFYDEBUG("REQ_TYPE_ALBUMBROWSE is not yet implemented\n");
-		return request_set_result(session, req, SP_ERROR_OTHER_PERMAMENT, NULL);
-	}
-	else if(req->type == REQ_TYPE_ARTISTBROWSE) {
-		req->next_timeout = get_millisecs() + BROWSE_RETRY_TIMEOUT*1000;
-		
-		DSFYDEBUG("REQ_TYPE_ARTISTBROWSE is not yet implemented\n");
-		return request_set_result(session, req, SP_ERROR_OTHER_PERMAMENT, NULL);
-	}
+	switch(req->type) {
+		case REQ_TYPE_BROWSE_TRACK:
+			/* Send request (CMD_BROWSE) to load track data. */
+			ret = browse_send_browsetrack_request(session, req, 0);
+			break;
 
+		case REQ_TYPE_BROWSE_ALBUM:
+		case REQ_TYPE_BROWSE_ARTIST:
+		case REQ_TYPE_ALBUMBROWSE:
+		case REQ_TYPE_ARTISTBROWSE:
+			DSFYDEBUG("Browsing for <type %s, state %s, input %p>\n", 
+				  REQUEST_TYPE_STR(req->type),
+				  REQUEST_STATE_STR(req->state), req->input);
+			ret = browse_send_generic_request(session, req);
+			break;
 
+		default:
+			DSFYDEBUG("BUG: <type %s> is not yet implemented\n", REQUEST_TYPE_STR(req->type));
+			ret = request_set_result(session, req, SP_ERROR_OTHER_PERMAMENT, NULL);
+			break;
+	}
+	
 	return ret;
+}
+
+
+static int browse_send_generic_request(sp_session *session, struct request *req) {
+	int ret;
+	struct browse_callback_ctx *brctx;
+	int i;
+	unsigned char *idlist;
+	int browse_type;
+	
+	brctx = *(struct browse_callback_ctx **)req->input;
+	
+	/* FIXME: Should probably move this one? */
+	brctx->req = req;
+	req->state = REQ_STATE_RUNNING;
+
+	
+	/* Are we done yet? */
+	if(brctx->num_browsed == brctx->num_total) {
+		DSFYDEBUG("Offset reached total count of %d, returning results for <type %s>!\n", brctx->num_total, REQUEST_TYPE_STR(req->type));
+		switch(brctx->type) {
+			case REQ_TYPE_ALBUMBROWSE:
+				ret = request_set_result(session, req, SP_ERROR_OK, brctx->data.albumbrowses[0]);
+			case REQ_TYPE_ARTISTBROWSE:
+				ret = request_set_result(session, req, SP_ERROR_OK, brctx->data.artistbrowses[0]);
+				break;
+			default:
+				ret = request_set_result(session, req, SP_ERROR_OK, NULL);
+				break;
+		}
+		
+		/* Free browse callback context */
+		free(brctx);
+		
+		return ret;
+	}
+
+
+	/* Don't send too many browse requests at once */
+	brctx->num_in_request = brctx->num_total - brctx->num_browsed;
+	if(brctx->num_in_request > MAX_TRACKS_PER_REQUEST)
+		brctx->num_in_request = MAX_TRACKS_PER_REQUEST;
+
+
+	/* Create list of album/artist/track IDs */
+	idlist = (unsigned char *)malloc(16 * brctx->num_in_request);
+	switch(brctx->type) {
+		case REQ_TYPE_ALBUMBROWSE:
+			browse_type = BROWSE_ALBUM;
+			for(i = 0; i < brctx->num_in_request; i++)
+				memcpy(idlist + i*16, brctx->data.albumbrowses[brctx->num_browsed + i]->album->id, 16);
+			break;
+
+		case REQ_TYPE_ARTISTBROWSE:
+			browse_type = BROWSE_ARTIST;
+			for(i = 0; i < brctx->num_in_request; i++)
+				memcpy(idlist + i*16, brctx->data.artistbrowses[brctx->num_browsed + i]->artist->id, 16);
+			break;
+			
+		case REQ_TYPE_BROWSE_ALBUM:
+			browse_type = BROWSE_ALBUM;
+			for(i = 0; i < brctx->num_in_request; i++)
+				memcpy(idlist + i*16, brctx->data.albums[brctx->num_browsed + i]->id, 16);
+			break;
+
+		case REQ_TYPE_BROWSE_ARTIST:
+			browse_type = BROWSE_ARTIST;
+			for(i = 0; i < brctx->num_in_request; i++)
+				memcpy(idlist + i*16, brctx->data.artists[brctx->num_browsed + i]->id, 16);
+			break;
+			
+		case REQ_TYPE_BROWSE_TRACK:
+			browse_type = BROWSE_TRACK;
+			for(i = 0; i < brctx->num_in_request; i++)
+				memcpy(idlist + i*16, brctx->data.tracks[brctx->num_browsed + i]->id, 16);
+			break;
+
+		default:
+			DSFYDEBUG("BUG: Can't handle <type %s>\n", REQUEST_TYPE_STR(req->type));
+			break;
+	}
+
+	
+	/* Buffer to hold data (gzip'd XML) retrieved */
+	assert(brctx->buf == NULL);
+	brctx->buf = buf_new();
+
+	
+	DSFYDEBUG("Sending BROWSE for %d items (at offset %d) on behalf of <type %s>\n",
+		  brctx->num_in_request, brctx->num_browsed, REQUEST_TYPE_STR(req->type));
+
+	ret = cmd_browse(session, browse_type, idlist, brctx->num_in_request, browse_generic_callback, brctx);
+	
+	free(idlist);
+	
+	return ret;
+}
+
+
+/* Callback for browse requests */
+static int browse_generic_callback(CHANNEL *ch, unsigned char *payload, unsigned short len) {
+	int skip_len;
+	struct browse_callback_ctx *brctx;
+	brctx = (struct browse_callback_ctx *)ch->private;
+
+	switch(ch->state) {
+		case CHANNEL_DATA:
+			/* Skip a minimal gzip header */
+			if (ch->total_data_len < 10) {
+				skip_len = 10 - ch->total_data_len;
+				while(skip_len && len) {
+					skip_len--;
+					len--;
+					payload++;
+				}
+				
+				if (len == 0)
+					break;
+			}
+			
+			buf_append_data(brctx->buf, payload, len);
+			break;
+			
+		case CHANNEL_ERROR:
+			DSFYDEBUG("Got a channel ERROR, freeing buffer and retrying within %d seconds\n", BROWSE_RETRY_TIMEOUT);
+			buf_free(brctx->buf);
+			brctx->buf = NULL;
+			
+			/* The request will be retried as soon as req->next_timeout expires */
+			break;
+			
+		case CHANNEL_END:
+			DSFYDEBUG("Got all data, calling parser\n");
+			brctx->browse_parser(brctx);
+			buf_free(brctx->buf);
+			brctx->buf = NULL;
+			
+			/* Increase number of items processed */
+			brctx->num_browsed += brctx->num_in_request;
+			
+			/* Force the next browse request to happen immediately */
+			brctx->req->next_timeout = 0;
+			
+			break;
+			
+		default:
+			break;
+	}
+	
+	return 0;
 }
 
 
--- libopenspotify/browse.h
+++ libopenspotify/browse.h
@@ -4,10 +4,50 @@
 #include <spotify/api.h>
 
 #include "buf.h"
+#include "hashtable.h"
 #include "request.h"
 
 
 #define BROWSE_RETRY_TIMEOUT	30
+
+
+struct browse_callback_ctx;
+typedef int (*browse_parser) (struct browse_callback_ctx *brctx);
+
+struct browse_callback_ctx {
+	/* Provides access to the session's hashtables among other things */
+	sp_session *session;
+
+	/* The request, so we can store the result */
+	struct request *req;
+	
+	/* For keeping the gzip'd XML */
+	struct buf *buf;
+
+	/* Type of objects, same as request->type */
+	int type;
+
+	union {
+		sp_playlist *playlist;
+		sp_artist **artists;
+		sp_album **albums;
+		sp_track **tracks;
+		sp_albumbrowse **albumbrowses;
+		sp_artistbrowse **artistbrowses;
+	} data;
+
+	/* Total number of objects in the above struct */
+	int num_total;
+
+	/* Total number of objects we've browsed so far */
+	int num_browsed;
+	
+	/* Number of items in the current request */
+	int num_in_request;
+	
+	/* Gzip'd XML parser, provided by the caller */
+	browse_parser browse_parser;
+};
 
 
 int browse_process(sp_session *session, struct request *req);
