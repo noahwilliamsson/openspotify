0c94556f610be583f17b7d38f6ee8492e8411968
Noah Williamsson <noah.williamsson@gmail.com>
2009-08-13 21:02:58 +0200

    Update debug.h in example/*/

--- examples/test/debug.h
+++ examples/test/debug.h
@@ -5,23 +5,21 @@
 #include <stdio.h>
 
 #ifdef _WIN32
-#define DSFYDEBUG(...) {                                        \
-        char *file = __FILE__, *ptr;                            \
-                                                                \
-        for(ptr = file; *ptr; ptr++);                           \
-        while(ptr != file && *ptr-- != '\\');                   \
-        if(ptr != file) ptr += 2;                               \
-                fprintf(stderr, "%s:%d %s() ", ptr, __LINE__,       \
-                        __FUNCTION__);                          \
-                fprintf(stderr, __VA_ARGS__);                       \
-	}							\
+#define DSFYDEBUG(...) {				\
+	char *file = __FILE__, *ptr;			\
+	for(ptr = file; *ptr; ptr++);			\
+	while(ptr != file && *ptr-- != '\\');		\
+	if(ptr != file) ptr += 2;			\
+	fprintf(stderr, "%s:%d %s() ", ptr, __LINE__,	\
+                        __FUNCTION__);			\
+	fprintf(stderr, __VA_ARGS__);			\
 }
 #else
 #include <libgen.h>
-#define DSFYDEBUG(...) {                                        \
-                fprintf(stderr, "%s:%d %s() ", basename(__FILE__), __LINE__,	\
-                        __func__);				\
-                fprintf(stderr, __VA_ARGS__);			\
+#define DSFYDEBUG(...) {						\
+	fprintf(stderr, "%s:%d %s() ", basename(__FILE__), __LINE__,	\
+			__func__);					\
+	fprintf(stderr, __VA_ARGS__);					\
 }
 #endif
 #else
