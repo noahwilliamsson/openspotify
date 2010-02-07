48abd89742fa558fdbd03d01c7e82e3ef657f86e
Noah Williamsson <noah.williamsson@gmail.com>
2010-02-07 13:26:02 +0100

    Implement toplist browsing

--- README.txt
+++ README.txt
@@ -31,7 +31,7 @@ http://developer.spotify.com/en/libspotify/docs/modules.html
 * Artist subsystem	- Completed
 * Album browsing	- Completed
 * Artist browsing	- Completed
-* Toplist browsing	- Stubs, untested
+* Toplist browsing	- Completed
 * Image handling	- Completed
 * Search subsysten	- Completed
 * Playlist subsystem	- Read-only (loading), no support for create/modify/remove playlists or tracks
--- libopenspotify/toplistbrowse.c
+++ libopenspotify/toplistbrowse.c
@@ -81,6 +81,9 @@ static int toplistbrowse_callback(CHANNEL *ch, unsigned char *payload, unsigned 
 
 			request_set_result(toplistbrowse_ctx->session, toplistbrowse_ctx->req, toplistbrowse_ctx->toplistbrowse->error, toplistbrowse_ctx->toplistbrowse);
 
+			/* Release reference made in sp_toplistbrowse_create() */
+			sp_toplistbrowse_release(toplistbrowse_ctx->toplistbrowse);
+
 			buf_free(toplistbrowse_ctx->buf);
 			free(toplistbrowse_ctx);
 			break;
@@ -95,9 +98,13 @@ static int toplistbrowse_callback(CHANNEL *ch, unsigned char *payload, unsigned 
 
 
 static int toplistbrowse_parse_xml(struct toplistbrowse_ctx *toplistbrowse_ctx) {
+	unsigned char id[16];
 	struct buf *xml;
 	sp_toplistbrowse *toplistbrowse = toplistbrowse_ctx->toplistbrowse;
-	ezxml_t root;
+	ezxml_t root, listnode, node;
+	sp_artist *artist;
+	sp_album *album;
+	sp_track *track;
 	
 	xml = despotify_inflate(toplistbrowse_ctx->buf->ptr, toplistbrowse_ctx->buf->len);
 	if(xml == NULL)
@@ -106,7 +113,9 @@ static int toplistbrowse_parse_xml(struct toplistbrowse_ctx *toplistbrowse_ctx) 
 #ifdef DEBUG
 	{
 		FILE *fd;
-		fd = fopen("toplistbrowse.xml", "w");
+		char buf[64];
+		sprintf(buf, "toplistbrowse-%04x-%04x.xml", toplistbrowse->type, toplistbrowse->region);
+		fd = fopen(buf, "w");
 		if(fd) {
 			fwrite(xml->ptr, xml->len, 1, fd);
 			fclose(fd);
@@ -114,7 +123,7 @@ static int toplistbrowse_parse_xml(struct toplistbrowse_ctx *toplistbrowse_ctx) 
 	}
 #endif
 
-	root = ezxml_parse_str((char *) xml->ptr, xml->len);
+	root = ezxml_parse_str((char *)xml->ptr, xml->len);
 	if(root == NULL) {
 		DSFYDEBUG("Failed to parse XML\n");
 		buf_free(xml);
@@ -122,10 +131,61 @@ static int toplistbrowse_parse_xml(struct toplistbrowse_ctx *toplistbrowse_ctx) 
 	}
 
 
+	for(listnode = ezxml_get(root, "artists", 0, "artist", -1);
+	    listnode;
+	    listnode = listnode->next) {
 
-	/* XXX - Figure this out! */
+		if((node = ezxml_get(listnode, "id", -1)) == NULL)
+			continue;
+		
+		hex_ascii_to_bytes(node->txt, id, 16);
+		artist = osfy_artist_add(toplistbrowse_ctx->session, id);
+		
+		if(!sp_artist_is_loaded(artist))
+			osfy_artist_load_artist_from_xml(toplistbrowse_ctx->session, artist, listnode);
+		
+		sp_artist_add_ref(artist);
+		toplistbrowse->artists = (sp_artist **)realloc(toplistbrowse->artists, (toplistbrowse->num_artists + 1) * sizeof(sp_artist *));
+		toplistbrowse->artists[toplistbrowse->num_artists++] = artist;
+	}
 
-	toplistbrowse->error = SP_ERROR_OTHER_PERMANENT;
+
+	for(listnode = ezxml_get(root, "albums", 0, "album", -1);
+	    listnode;
+	    listnode = listnode->next) {
+
+		if((node = ezxml_get(listnode, "id", -1)) == NULL)
+			continue;
+		
+		hex_ascii_to_bytes(node->txt, id, 16);
+		album = sp_album_add(toplistbrowse_ctx->session, id);
+		
+		if(!sp_album_is_loaded(album))
+			osfy_album_load_from_search_xml(toplistbrowse_ctx->session, album, listnode);
+		
+		sp_album_add_ref(album);
+		toplistbrowse->albums = (sp_album **)realloc(toplistbrowse->albums, (toplistbrowse->num_albums + 1) * sizeof(sp_album *));
+		toplistbrowse->albums[toplistbrowse->num_albums++] = album;
+	}
+
+
+	for(listnode = ezxml_get(root, "tracks", 0, "track", -1);
+	    listnode;
+	    listnode = listnode->next) {
+
+		if((node = ezxml_get(listnode, "id", -1)) == NULL)
+			continue;
+		
+		hex_ascii_to_bytes(node->txt, id, 16);
+		track = osfy_track_add(toplistbrowse_ctx->session, id);
+		
+		if(!sp_track_is_loaded(track))
+			osfy_track_load_from_xml(toplistbrowse_ctx->session, track, listnode);
+		
+		sp_track_add_ref(track);
+		toplistbrowse->tracks = (sp_track **)realloc(toplistbrowse->tracks, (toplistbrowse->num_tracks + 1) * sizeof(sp_track *));
+		toplistbrowse->tracks[toplistbrowse->num_tracks++] = track;
+	}
 
 
 	ezxml_free(root);
